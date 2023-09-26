fadb3376761c01fcda025fc76a0a5c13b508f50c
olafbond <52509158+olafbond@users.noreply.github.com>
2023-09-26 11:28:39 +0300

    Update Workflowy calendar.py
    Added two languages support.
    Added week lines.
    Switched to isocalendar() method to get week number and week day.
    Simplified and cleaned.

--- Workflowy calendar.py
+++ Workflowy calendar.py
@@ -3,12 +3,23 @@ from datetime import timedelta, date
 import locale
 
 # Settings
-year = 2024  # For which year would you like to generate a calendar?
-month_lines = True  # Add months lines inline?
-day_notes = True  # Add notes to dates for journaling?
-note_string = '_note="#Goals &#10;#Achievements &#10;#Knowledge &#10;#Journal "'
-LOCALE = 'en'  # Local variables https://www.localeplanet.com/icu/
+year = 2024  # Calendar's date
+month_lines = True  # Add months lines inline
+week_lines = True  # Add week lines inline
+week_day_start = 1  # 1 - Monday, ... 7 - Sunday
+day_notes = True  # Add notes to dates for journaling
+
+display_year = '%y'
+display_month = '%y%m'
 display_date = '%y%m%d'  # DateFormat https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes
+
+LOCALE = 'en'  # 'en'. Local variables https://www.localeplanet.com/icu/
+if LOCALE == 'ru':
+    note_string = '_note="#Цели &#10;#Преодоление &#10;#Знания &#10;#Журнал "'
+    week_word = 'Неделя'
+else:
+    note_string = '_note="#Goals &#10;#Achievements &#10;#Knowledge &#10;#Journal "'
+    week_word = 'Week'
 
 # Don't change anything after this line
 locale.setlocale(locale.LC_ALL, LOCALE)
@@ -19,23 +30,24 @@ def daterange(s_date, e_date):
         yield s_date + timedelta(n)
 
 
-ye = year - 2000
+start_date = date(year, 1, 1)
+end_date = date(year + 1, 1, 1)
 
 html = f'<?xml version="1.0"?>\n'
 html += f'<opml version="2.0"><body>\n'
-html += f'<outline text="&lt;b&gt;{ye}&lt;/b&gt;">\n'  # year's string
-
-start_date = date(year, 1, 1)
-end_date = date(year + 1, 1, 1)
+html += f'<outline text="&lt;b&gt;{start_date.strftime(display_year)}&lt;/b&gt;">\n'  # year's short string
 
 for single_date in daterange(start_date, end_date):
 
     if month_lines and single_date.day == 1:  # month's string
-        html += f'<outline text="&lt;b&gt;{single_date.strftime("%y%m")}&lt;/b&gt;"/>\n'
+        html += f'<outline text="&lt;b&gt;{single_date.strftime(display_month)}&lt;/b&gt;"/>\n'
+
+    if week_lines and single_date.isocalendar()[2] == week_day_start:  # weeks's string
+        html += f'<outline text="{week_word} {single_date.isocalendar()[1]}"/>\n'
 
     DateOfWeek = single_date.strftime(display_date) + ' ' + single_date.strftime("%a")  # day's string
 
-    if single_date.strftime("%w") != '0' and single_date.strftime("%w") != '6':  # working days
+    if single_date.isocalendar()[2] != 6 and single_date.isocalendar()[2] != 7:  # working days
         html += f'<outline text="{DateOfWeek}"'
     else:  # weekend
         html += f'<outline text="&lt;span class=&quot;colored c-pink&quot;&gt;{DateOfWeek}&lt;/span&gt;"'
