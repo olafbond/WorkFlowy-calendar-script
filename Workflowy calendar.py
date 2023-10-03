4e4fce2e8f7bc67270221ed4529144e7dad32ca4
olafbond <52509158+olafbond@users.noreply.github.com>
2023-10-04 02:20:46 +0300

    Update Workflowy calendar.py
    Native WF dates are included in the calendar's dates.

--- Workflowy calendar.py
+++ Workflowy calendar.py
@@ -5,7 +5,7 @@ import locale
 # Settings
 year = 2024  # Calendar's date
 month_lines = True  # Add months lines inline
-week_lines = True  # Add week lines inline
+week_lines = False  # Add week lines inline
 week_day_start = 1  # 1 - Monday, ... 7 - Sunday
 day_notes = True  # Add notes to dates for journaling
 
@@ -13,12 +13,12 @@ display_year = '%y'
 display_month = '%y%m'
 display_date = '%y%m%d'  # DateFormat https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes
 
-LOCALE = 'en'  # 'en'. Local variables https://www.localeplanet.com/icu/
+LOCALE = 'ru'  # 'en'. Local variables https://www.localeplanet.com/icu/
 if LOCALE == 'ru':
-    note_string = '_note="#Цели &#10;#Преодоление &#10;#Знания &#10;#Журнал "'
+    note_string = '_note="#Цели &#10;#Преодоление &#10;#Знания &#10;#Журнал '
     week_word = 'Неделя'
 else:
-    note_string = '_note="#Goals &#10;#Achievements &#10;#Knowledge &#10;#Journal "'
+    note_string = '_note="#Goals &#10;#Achievements &#10;#Knowledge &#10;#Journal '
     week_word = 'Week'
 
 # Don't change anything after this line
@@ -35,7 +35,7 @@ end_date = date(year + 1, 1, 1)
 
 html = f'<?xml version="1.0"?>\n'
 html += f'<opml version="2.0"><body>\n'
-html += f'<outline text="&lt;b&gt;{start_date.strftime(display_year)}&lt;/b&gt;">\n'  # year's short string
+html += f'<outline text="&lt;b&gt;{start_date.strftime(display_year)}&lt;/b&gt;"/>\n'  # year's short string
 
 for single_date in daterange(start_date, end_date):
 
@@ -47,16 +47,24 @@ for single_date in daterange(start_date, end_date):
 
     DateOfWeek = single_date.strftime(display_date) + ' ' + single_date.strftime("%a")  # day's string
 
-    if single_date.isocalendar()[2] != 6 and single_date.isocalendar()[2] != 7:  # working days
-        html += f'<outline text="{DateOfWeek}"'
-    else:  # weekend
-        html += f'<outline text="&lt;span class=&quot;colored c-pink&quot;&gt;{DateOfWeek}&lt;/span&gt;"'
-
+    html += f'<outline text="&lt;'
+    html += f'time '
+    html += f'startYear=&quot;{single_date.strftime("%Y")}&quot; '
+    html += f'startMonth=&quot;{single_date.strftime("%m")}&quot; '
+    html += f'startDay=&quot;{single_date.strftime("%d")}&quot;'
+    if single_date.isocalendar()[2] == 6 or single_date.isocalendar()[2] == 7:  # weekend
+        html += f'&gt;&lt;span class=&quot;colored c-pink&quot;'
+        html += f'&gt;{DateOfWeek}&lt;'
+        html += f'/span&gt;&lt;'
+        html += '/time&gt;"'
+#        html += '&lt;span class=&quot;colored bc-pink&quot;&gt; &lt;/span&gt;" />\n'
+    else:
+        html += f'&gt;{DateOfWeek}&lt;'
+        html += '/time&gt;"'
     if day_notes:
         html += ' ' + note_string
+    html += '" />\n'
 
-    html += ' />\n'
-
-html += '</outline></body></opml>'
+html += '</body></opml>'
 
 clipboard.copy(html)
